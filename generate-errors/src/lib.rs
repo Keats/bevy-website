8110c9aced2ebc9cd95085f2af3cc6be4e40bda8
Vincent Prouillet <github@vincentprouillet.com>
2026-09-24 23:22:09 +0200

    Add syntax highlighting for bevy errors

--- generate-errors/src/lib.rs
+++ generate-errors/src/lib.rs
@@ -67,47 +67,9 @@ pub fn get_error_pages(bevy_errors_path: &Path) -> anyhow::Result<HashMap<String
         // The error pages already have a header built-in
         // but Zola provides its own title header
         // so we need to remove this for proper formatting
-        let regex_content = regex.replace(content.as_str(), "");
+        let content = regex.replace(content.as_str(), "").into_owned();
 
-        // Code blocks will be invalid unless
-        // the annotations are before the language
-        // like `should_panic,rust` or `no_run,rust`.
-        let mut content = String::new();
-        for line in regex_content.lines() {
-            // Ensure we only operate on
-            // Rust code blocks.
-            if !line.starts_with("```rust") {
-                content.push_str(line);
-                content.push('\n');
-                continue;
-            }
-
-            let annotations = line
-                .strip_prefix("```")
-                .ok_or(anyhow!(
-                    "Failed to find start of code block to strip from string"
-                ))?
-                .split(',');
-
-            let mut line: String = String::from("```");
-
-            // Add all annotations other than rust
-            // to the content first to avoid the issue.
-            for annotation in annotations {
-                if annotation == "rust" {
-                    continue;
-                }
-
-                line.push_str(annotation);
-                line.push(',');
-            }
-            line.push_str("rust");
-
-            content.push_str(&line);
-            content.push('\n');
-        }
-
-        results_map.insert(file_name, content.to_string());
+        results_map.insert(file_name, content);
     }
 
     Ok(results_map)
